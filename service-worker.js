06018c94aa5e02689b26646993d9d46351afd357
LROMERO8 <lromero_rsx@hotmail.com>
2021-03-29 22:15:49 -0400

    service worker code reverted to NOT use importa statements

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,20 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 
-const {registerRoute} = workbox.routing;
-const {CacheFirst} = workbox.strategies;
-const {CacheableResponse} = workbox.cacheableResponse;
+// This will trigger the importScripts() for workbox.strategies and its dependencies:
+workbox.loadModule('workbox-strategies');
 
-// Cache images with a Cache First strategy
-registerRoute(
-    // Check to see if the request's destination is style for an image
-    ({ request }) => request.destination === 'image',
-    // Use a Cache First caching strategy
-    new CacheFirst({
-      // Put all cached files in a cache named 'images'
-      cacheName: 'images',
-      plugins: [
-        // Ensure that only requests that result in a 200 status are cached
-        new CacheableResponsePlugin({
-          statuses: [200],
-        }),
-        // Don't cache more than 50 items, and expire them after 30 days
-        new ExpirationPlugin({
-          maxEntries: 50,
-          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
-        }),
-      ],
-    }),
-);
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.endsWith('.png')) {
+    // Referencing workbox.strategies will now work as expected.
+    const cacheFirst = new workbox.strategies.CacheFirst();
+    event.respondWith(cacheFirst.handle({request: event.request}));
+  }
+});
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.CacheFirst()
+  );
 
 // Updated offline detection logic
 // That the installed service worker fetch event returns an HTTP 200 status code (indicating a successful fetch) in simulated offline mode.
