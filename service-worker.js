e8a6d80c594b608b9641a5a04a51d543a30a6118
LROMERO8 <lromero_rsx@hotmail.com>
2021-03-29 22:11:24 -0400

    service worker code converted to use import statements 2.0

--- service-worker.js
+++ service-worker.js
@@ -4,13 +4,26 @@ const {registerRoute} = workbox.routing;
 const {CacheFirst} = workbox.strategies;
 const {CacheableResponse} = workbox.cacheableResponse;
 
+// Cache images with a Cache First strategy
 registerRoute(
-  ({request}) => request.destination === 'image',
-  new CacheFirst({
-    plugins: [
-      new CacheableResponsePlugin({statuses: [0, 200]})
-    ],
-  })
+    // Check to see if the request's destination is style for an image
+    ({ request }) => request.destination === 'image',
+    // Use a Cache First caching strategy
+    new CacheFirst({
+      // Put all cached files in a cache named 'images'
+      cacheName: 'images',
+      plugins: [
+        // Ensure that only requests that result in a 200 status are cached
+        new CacheableResponsePlugin({
+          statuses: [200],
+        }),
+        // Don't cache more than 50 items, and expire them after 30 days
+        new ExpirationPlugin({
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+        }),
+      ],
+    }),
 );
 
 // Updated offline detection logic
