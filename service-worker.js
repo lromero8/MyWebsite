08cb269faeb9863154fee4a0209a0025a88f46d4
LROMERO8 <lromero_rsx@hotmail.com>
2021-03-29 21:59:05 -0400

    adding fetch event listener

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,20 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 
+// This will trigger the importScripts() for workbox.strategies and its dependencies:
+workbox.loadModule('workbox-strategies');
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.endsWith('.png')) {
+    // Referencing workbox.strategies will now work as expected.
+    const cacheFirst = new workbox.strategies.CacheFirst();
+    event.respondWith(cacheFirst.handle({request: event.request}));
+  }
+});
+
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.CacheFirst()
   );
+
+// Updated offline detection logic
+// That the installed service worker fetch event returns an HTTP 200 status code (indicating a successful fetch) in simulated offline mode.
